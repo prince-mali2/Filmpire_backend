3bd5b5577ab3e0347c78eec7641c3d3f029cb512
prince-mali2 <princemali019@gmail.com>
2025-05-21 16:34:35 +0530

    login added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+import express from "express";
+import axios from "axios";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
